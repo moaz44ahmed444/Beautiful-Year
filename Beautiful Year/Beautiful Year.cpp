02a731a4b3965207275919db24406c5603ad45bd
Moaz Ahmed <Moaz_201900836@fci.helwan.edu.eg>
2020-10-10 13:08:47 +0200

    new project

---
+++ Beautiful Year/Beautiful Year.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+bool BeautifulYear(int year) {
+	int arr[1000], length = 0;
+	while (year !=0) {
+		arr[length]= year % 10;
+		year = year / 10;
+		length++;
+	}
+	
+	for (int j = 0; j < length-1; j++) {
+		for (int i = j + 1; i < length; i++) {
+			if (arr[j] == arr[i]) {
+				return false;
+			}
+		}
+	}
+	return true;
+
+}
+int main()
+{
+	int year;
+	cin >> year;
+	++year;
+	while (BeautifulYear(year) == false)
+	{
+		++year;
+	}
+	cout << year;
+}
